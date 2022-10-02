58f83010130ffdc8649d95dfdae4d8e7ee8aae50
kellock117 <92283523+kellock117@users.noreply.github.com>
2022-10-03 02:48:34 +0800

    Update apolloClient.tsx

--- client/src/apolloClient.tsx
+++ client/src/apolloClient.tsx
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3000/",
+  uri: "https://simcomm.netlify.app/",
 });
 
 const authLink = setContext(() => {
