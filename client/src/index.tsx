8289779a94b015f1a0289ff67d731d63d31e6568
Tommy <jin31022324@gmail.com>
2022-10-03 04:01:49 +0800

    bug

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import client from "./apolloClient";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/authContext.js";
+import { AuthProvider } from "./context/authContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
